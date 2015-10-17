ea76b872478ed8144075b300ffd50df721bae3d3
ironholds <ironholds@gmail.com>
2015-10-17 12:03:11 -0400

    add historic and current conversion rates

--- R/api.R
+++ R/api.R
@@ -1,8 +1,11 @@
 #'@importFrom httr GET user_agent stop_for_status content
-oer_query <- function(path, key, ...){
+oer_query <- function(path, key, args = NULL, ...){
   
   # Construct URL
   url <- paste0("https://openexchangerates.org/api/", path, "?app_id=", key)
+  if(!is.null(args)){
+    url <- paste0(url, "&", args)
+  }
   
   # Make request
   result <- httr::GET(url, httr::user_agent("rrency - https://github.com/Ironholds/rrency"))
@@ -23,7 +26,7 @@ oer_query <- function(path, key, ...){
 #'
 #'@param x a vector of amounts, formatted as integer or numeric values.
 #'
-#'@param from the currency \code{x} is currently in.
+#'@param from the currency \code{x} is currently in. US Dollars by default.
 #'
 #'@param to the currency you \emph{want} \code{x} to be in.
 #'
@@ -33,7 +36,7 @@ oer_query <- function(path, key, ...){
 #'@param ... further arguments to pass to httr's \code{GET} function.
 #'
 #'@export
-currency_convert <- function(x, from, to, key, ...){
+currency_convert <- function(x, from = "USD", to, key, ...){
   
   # Check the data is integer/numeric
   if(!any(is.numeric(x), is.integer(x))){
@@ -41,7 +44,7 @@ currency_convert <- function(x, from, to, key, ...){
   }
   
   # Request the current conversion rates
-  rates <- oer_query("latest.json", key, ...)
+  rates <- oer_query("latest.json", key, NULL, ...)
   
   # Check we have the currency
   conversion_rate <- rates$rates[[to]]
@@ -77,11 +80,72 @@ currency_convert <- function(x, from, to, key, ...){
 #'
 #'@export
 list_currencies <- function(key, as_df = FALSE, ...){
-  results <- oer_query("currencies.json", key, ...)
+  results <- oer_query("currencies.json", key, NULL, ...)
   if(as_df){
     return(data.frame(code = names(results),
                       name = unlist(results),
                       stringsAsFactors = FALSE))
   }
   return(results)
+}
+
+#'@title Get Historic Currency Data
+#'
+#'@param dates a vector of dates to get data for. Strings or date objects.
+#'
+#'@param currency the currency to use as a baseline. By default this is US Dollars; while it can
+#'be changed to other currencies, that does require an 'Enterprise' OER key, and will otherwise
+#'fail.
+#'
+#'@param key Your Open Exchange Rates API key. See their \href{https://openexchangerates.org/signup}{access plans}
+#'(particularly the 'forever free' plan linked at the bottom).
+#'
+#'@param ... Further arguments to pass to httr's \code{GET} function.
+#'
+#'@export
+historic_currency <- function(dates, currency = "USD", key, ...){
+  
+  # Make sure the dates are strings
+  dates <- as.character(dates)
+  
+  # If there's more than one, recurse.
+  if(length(dates) > 1){
+    return(lapply(dates, historic_currency, currency = currency, key = key, ...))
+  }
+  
+  # Otherwise construct a path
+  path <- paste0("historical/", dates, ".json")
+  
+  # Make the query and return
+  if(currency == "USD"){
+    args <- NULL
+  } else {
+    args <- paste0("base=", currency)
+  }
+  return(oer_query(path, key, args, ...))
+}
+
+#'@title Get Current Conversion Rates
+#'
+#'@description Get the current conversion rates between various different currencies.
+#'
+#'@param currency The currency to use as a baseline. By default this is US Dollars; while it can
+#'be changed to other currencies, that does require an 'Enterprise' OER key, and will otherwise
+#'fail.
+#'
+#'@param key Your Open Exchange Rates API key. See their \href{https://openexchangerates.org/signup}{access plans}
+#'(particularly the 'forever free' plan linked at the bottom).
+#'
+#'@param ... Further arguments to pass to httr's \code{GET} function.
+#'
+#'@export
+conversion_rates <- function(currency = "USD", key, ...){
+  
+  if(currency == "USD"){
+    args <- NULL
+  } else {
+    args <- paste0("base=", currency)
+  }
+  
+  return(oer_query(path = "latest.json", key = key, args = args, ...))
 }
